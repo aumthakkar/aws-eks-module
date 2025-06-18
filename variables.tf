2bf9e35ac208cb862006ff89a9a685fd766ab883
aumthakkar <aumthakkar@yahoo.co.uk>
2025-06-18 20:40:20 +0100

    modified root variables.tf and eks/variables.tf appropriately

--- variables.tf
+++ variables.tf
@@ -9,8 +9,5 @@ variable "aws_region" {}
 variable "org_name" {}
 variable "env_name" {}
 
-variable "auto_create_subnet_address" {
-  type = bool
-  description = "To get decision about auto creating subnet cidr addresses"
-}
+
 
