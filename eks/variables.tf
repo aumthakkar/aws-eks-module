84db2e1b0dbf19656fb4b6acf43f4398a651a41a
aumthakkar <aumthakkar@yahoo.co.uk>
2025-06-18 21:12:43 +0100

    further modified eks/variables.tf appropriately

--- eks/variables.tf
+++ eks/variables.tf
@@ -70,11 +70,8 @@ variable "aws_region" {
 variable "vpc_cidr" {}
 
 variable "public_subnet_count" {}
-variable "public_subnet_cidr" {}
-
 
 variable "private_subnet_count" {}
-variable "private_subnet_cidr" {}
 
 
 variable "cluster_security_groups" {}
