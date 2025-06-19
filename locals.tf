a0da4b12b4dcecbb22ea594ec10fe21eebdfc7b1
aumthakkar <aumthakkar@yahoo.co.uk>
2025-06-19 16:44:19 +0100

    get all source code out of eks folder into the root folder

--- locals.tf
+++ locals.tf
@@ -1,51 +1,36 @@
-
-# === root/locals.tf ===
-
-locals {
-
-  owners      = var.org_name
-  environment = var.env_name
-  name        = "${var.org_name}-${var.env_name}"
-
-}
-
-locals {
-  common_tags = {
-    owners      = local.owners
-    environment = local.environment
-  }
-}
-
-
 locals {
   public_subnet_cidr  = [for i in range(2, 255, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
   private_subnet_cidr = [for i in range(1, 255, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
 }
 
+locals {
+  public_subnet_cidr_block  = var.auto_create_subnet_address ? local.public_subnet_cidr : var.public_subnet_cidr_addresses
+  private_subnet_cidr_block = var.auto_create_subnet_address ? local.private_subnet_cidr : var.private_subnet_cidr_addresses
+}
 
 locals {
   cluster_security_groups = {
     public_sg = {
-      name        = "public-sg"
-      description = "public-security-group"
+      name        = var.cluster_public_security_groups_name
+      description = var.cluster_public_security_groups_desc
       tags = {
-        Name = "${local.name}-public-sg"
+        Name = "public-sg"
       }
       ingress = {
         ssh = {
           from        = 22
           to          = 22
           protocol    = "tcp"
-          cidr_blocks = var.access_ips
+          cidr_blocks = var.ssh_access_ips
         }
       }
     }
 
     efs_sg = {
-      name        = "efs-sg"
-      description = "vpc-access-to-efs"
+      name        = var.cluster_efs_security_group_name
+      description = var.cluster_efs_security_group_desc
       tags = {
-        Name = "${local.name}-efs-sg"
+        Name = "efs-sg"
       }
       ingress = {
         ssh = {
